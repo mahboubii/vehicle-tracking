536c4051a0543d1aed4259b8f490295a4090d69a
Amin Mahboubi <aminmahboobi@gmail.com>
2018-11-08 21:38:01 +0100

    j

--- backend-api/src/server.js
+++ backend-api/src/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config('.env');
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
@@ -9,25 +9,24 @@ const config = require('./config');
 
 const app = express();
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(jwt.jwt());
-
 
 app.use(function catchAuthErrors(err, req, res, next) {
 	if (err.name === 'UnauthorizedError') {
-		res.status(401).send({ success: false, data: 'Missing authentication credentials.'});
-	}else{
-    res.status(404).send({ success: false, data: 'Resource not found.'});
-  }
+		res.status(401).send({ success: false, data: 'Missing authentication credentials.' });
+	} else {
+		res.status(404).send({ success: false, data: 'Resource not found.' });
+	}
 });
 
 fs.readdirSync(path.join(__dirname, 'routes')).map(file => {
-  require('./routes/' + file)(app, '/api');
+	require('./routes/' + file)(app, '/api');
 });
 
 const server = app.listen(config.server.port, () => {
-    console.log(`Server is listening on http://localhost:${server.address().port}`);
-  });
+	console.log(`Server is listening on http://localhost:${server.address().port}`);
+});
